30f7bee10d674cbe376cd14480f96328ca184e6b
Ben <bengeraghty.bg@gmail.com>
2023-03-31 15:28:55 +0700

    add error-message

--- js/usersearch.js
+++ js/usersearch.js
@@ -2,6 +2,7 @@ const searchForm = document.getElementById('search-form');
 const usernameInput = document.getElementById('user-input');
 const username = document.getElementById('profile-name');
 const searchButton = document.getElementById('search');
+const errorEl = document.getElementById('error-message');
 
 const profilePicture = document.getElementById('profile-picture');
 const profileUsername = document.getElementById('username');
@@ -19,33 +20,56 @@ const website = document.getElementById('website')
 
 
 function fetchUserData() {
-  const usernameValue = usernameInput.value;
-  fetch(`https://api.github.com/users/${usernameValue}`)
-    .then(res => res.json())
-    .then(data => {
-         
-      profilePicture.src = data.avatar_url;
-      profilePicture.alt = `${data.login} avatar`;
-      username.innerHTML = `${data.name}`;
-      profileUsername.innerHTML = `${data.login}`;
-      biography.innerHTML = data.bio ? `${data.bio}` : 'No bio';
-      repoNumber.innerHTML = `${data.public_repos}`;
-      followerNumber.innerHTML = `${data.followers}`;
-      followingNumber.innerHTML = `${data.following}`;
-      joinedDate.innerHTML = formatDate(data.created_at);
-      locationEl.innerHTML = data.location ? `${data.location}` : '<span class="na">Not available</span>';
-      website.innerHTML = data.blog ? `<a href="${/^https?:\/\//i.test(data.blog) ? data.blog : 'https://' + data.blog}" target="_blank">${data.blog}</a>` : '<span class="na">Not available</span>';
-      twitter.innerHTML = data.twitter_username ? `<a href="https://twitter.com/${data.twitter_username}" target="_blank">${data.twitter_username}</a>` : '<span class="na">Not available</span>';
-      company.innerHTML = data.company ? `<a href="https://github.com/${data.company}" target="_blank">${data.company}</a>` : '<span class="na">Not available</span>';
-   
+  
+  const usernameValue = usernameInput.value.trim();;
+  if (!usernameValue) {
+    errorEl.style.display = 'block';
+    usernameInput.style.width = "47%";
+    return;
+  }
 
-      console.log(data);
+  if (errorEl.style.display === 'block') {
+    errorEl.style.display = 'none';
+    // usernameInput.style.width = "60%";
+  }
+
+  fetch(`https://api.github.com/users/${usernameValue}`)
+  
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+                
+      }
+      return res.json();
+    })
+    .then(data => {
+      usernameInput.style.width = "60%";
+      if (data && data.login) {
+        profilePicture.src = data.avatar_url;
+        profilePicture.alt = `${data.login} avatar`;
+        username.innerHTML = `${data.name}`;
+        profileUsername.innerHTML = `${data.login}`;
+        biography.innerHTML = data.bio ? `${data.bio}` : 'No bio';
+        repoNumber.innerHTML = `${data.public_repos}`;
+        followerNumber.innerHTML = `${data.followers}`;
+        followingNumber.innerHTML = `${data.following}`;
+        joinedDate.innerHTML = formatDate(data.created_at);
+        locationEl.innerHTML = data.location ? `${data.location}` : '<span class="na">Not available</span>';
+        website.innerHTML = data.blog ? `<a href="${/^https?:\/\//i.test(data.blog) ? data.blog : 'https://' + data.blog}" target="_blank">${data.blog}</a>` : '<span class="na">Not available</span>';
+        twitter.innerHTML = data.twitter_username ? `<a href="https://twitter.com/${data.twitter_username}" target="_blank">${data.twitter_username}</a>` : '<span class="na">Not available</span>';
+        company.innerHTML = data.company ? `<a href="https://github.com/${data.company}" target="_blank">${data.company}</a>` : '<span class="na">Not available</span>';
+
+        console.log(data);
+      }
     })
     .catch(error => {
       console.error(error);
-      alert('There was an error with your request. Please check your spelling or try again later.');
+      errorEl.style.display = 'block';
+      console.log('error, invalid input')
+      usernameInput.style.width = "47%";
     });
 }
+
 
 searchForm.addEventListener('submit', fetchUserData);
 searchButton.addEventListener('click', fetchUserData);
@@ -74,28 +98,3 @@ function getNumberSuffix(day) {
 
 
 
-// REMOVED
-
-      // website.innerHTML = data.blog ? `<a href="${data.blog}" target="_blank">${data.blog}</a>` : 'Not available';
-
-      // searchForm.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   fetchUserData();
-// });
-
-// searchButton.addEventListener('click', () => {
-//   fetchUserData();
-// });
-
-// usernameInput.addEventListener('keydown', (e) => {
-//   if (e.key === 'Enter') {
-//     e.preventDefault();
-//     fetchUserData();
-//   }
-// });
-
-// CHANGE DATE FORMAT
-
-// function handleSubmit() {
-//   fetchUserData();
-// }
